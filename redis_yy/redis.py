81f87b98016ddab1e5691219f675ce336e16f67a
yy <1023767856@qq.com>
2019-09-27 23:05:03 +0800

    change the return value

--- redis_yy/redis.py
+++ redis_yy/redis.py
@@ -7,7 +7,7 @@ import warnings
 
 import redis
 
-from redis_yy.lib.tool import Tool
+from .lib.tool import Tool
 
 __all__ = ['RedisDB']
 
@@ -19,13 +19,22 @@ class RedisDB(object):
     借鉴了flask的组件化思想，进行组件化开发) 会有一个 init_db函数
     """
 
-    def __init__(self, helper=None, **kwargs):
+    def __init__(self, helper=None, is_debug=True, **kwargs):
+        """
+        构造函数
+        :param max_conn: 最大连接数
+        :param mode: 连接模式  1 为默认连接池模式, 2 为单连接模式
+        :param helper:
+        :param kwargs:
+        """
         self.tool = Tool()
         self.redis = None
         self.host = kwargs.setdefault("host", "127.0.0.1")
         self.port = kwargs.setdefault("port", 6379)
         self.max_conn = kwargs.setdefault("max_conn", 1)
         self.password = kwargs.setdefault("password", None)
+        self.mode = kwargs.setdefault("mode", 1)
+        self.is_debug = is_debug
         if helper is not None:
             self.helper = helper
             self.init_helper(helper)
@@ -44,8 +53,11 @@ class RedisDB(object):
         Create a redis connection
         :return:
         """
-        pool = redis.ConnectionPool(host=self.host, port=self.port, db=self.max_conn, password=self.password)
-        self.redis = redis.Redis(connection_pool=pool)
+        if self.mode == 1:
+            pool = redis.ConnectionPool(host=self.host, port=self.port, db=self.max_conn, password=self.password)
+            self.redis = redis.Redis(connection_pool=pool)
+        else:
+            self.redis = redis.Redis(host=self.host, port=self.port, db=1, password=self.password)
 
     def get_redis(self):
         """
@@ -74,6 +86,8 @@ class RedisDB(object):
         self.port = self.helper.config[config].setdefault("PORT", 6379)
         self.max_conn = self.helper.config[config].setdefault("MAX_CONN", 1)
         self.password = self.helper.config[config].setdefault("PASSWORD", None)
+        self.mode = self.helper.config[config].setdefault("MODE", 1)
+        self.is_debug = self.helper.config[config].setdefault("DEBUG", True)
 
     def __call__(self, *args, **kwargs):
         """
@@ -126,3 +140,36 @@ class RedisDB(object):
         :return:
         """
         self.redis.close()
+
+    def set(self, name, value, ex=None, px=None, nx=False, xx=False):
+        """
+        设置给定key的值
+        Set key's value
+        :param name:
+        :param value:
+        :param ex:
+        :param px:
+        :param nx:
+        :param xx:
+        :return:
+        """
+        return self.redis.set(name, value, ex, px, nx, xx)
+
+    def get(self, key=None, encode="utf-8"):
+        """
+        获取对应键的值
+        Get value
+        :param encode:
+        :param key:
+        :return:
+        """
+        if key is None:
+            self.tool.debug("Please give a key which you want to search.")
+            return
+        try:
+            data = self.redis.get(key).decode(encode)
+        except Exception as e:
+            data = None
+            if self.is_debug:
+                self.tool.debug("Get key's value is error: ".format(error=e))
+        return data
