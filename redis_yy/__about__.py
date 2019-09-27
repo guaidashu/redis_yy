4ac530baa5d8fe4ff4c646cda8d489c7e48dc440
yy <1023767856@qq.com>
2019-09-27 19:53:16 +0800

    initialize

---
+++ redis_yy/__about__.py
@@ -0,0 +1,16 @@
+"""
+Create by yy on 2019/9/27
+"""
+
+__title__ = 'redis-yy'
+__description__ = 'A tool which is used to connect redis, designed by yy'
+__url__ = 'https://github.com/guaidashu/psql_yy'
+__version_info__ = ('0', '0', '1')
+__version__ = '.'.join(__version_info__)
+__author__ = 'guaidashu'
+__author_email__ = 'song42960@gmail.com'
+__maintainer__ = 'YY blog'
+__license__ = 'MIT'
+__copyright__ = '(c) 2019 by guaidashu'
+__install_requires__ = [
+]
---
+++ redis_yy/redis.py
@@ -0,0 +1,11 @@
+"""
+Create by yy on 2019/9/27
+"""
+
+
+class RedisDB(object):
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
